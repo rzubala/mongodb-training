e9f9b18fce99a0220da139e63df16beab922e3d6
Rafał Zubala <rzubala@operatus.org>
2022-02-08 16:06:07 +0100

    README

--- data/aggregate-rza.js
+++ data/aggregate-rza.js
@@ -22,12 +22,15 @@ db.persons.aggregate([
 ]);
 
 db.persons.aggregate([
-  {$project: {_id: 0, name: 1, email: 1, location: {type: "Point", 
+  {$project: {_id: 0, name: 1, email: 1, 
+    birthdate: {$convert: {input: "$dob.date", to: "date"}},
+    age: "$dob.age",
+    location: {type: "Point", 
     coordinates: [
       { $convert: {input: "$location.coordinates.longitude", to: "double", onError: 0.0, onNull: 0.0}}, 
       { $convert: {input: "$location.coordinates.latitude", to: "double", onError: 0.0, onNull: 0.0}}, 
     ]}}},
-  {$project: {gender: 1, email: 1, location: 1,
+  {$project: {gender: 1, email: 1, location: 1, birthdate: 1, age: 1,
     fullName: {$concat: 
     [
       { $toUpper: { $substrCP: ["$name.first", 0, 1] } },
