d780bde521e7e7acef60e005febe9da7c244f637
Rafał Zubala <rzubala@operatus.org>
2022-02-11 16:09:07 +0100

    pagination

--- app/backend/routes/products.js
+++ app/backend/routes/products.js
@@ -8,7 +8,7 @@ const router = Router();
 
 const products = [
   {
-    _id: 'fasdlk1j',
+    //_id: 'fasdlk1j',
     name: 'Stylish Backpack',
     description:
       'A stylish backpack for the modern women or men. It easily fits all your stuff.',
@@ -16,7 +16,7 @@ const products = [
     image: 'http://localhost:3100/images/product-backpack.jpg'
   },
   {
-    _id: 'asdgfs1',
+    //_id: 'asdgfs1',
     name: 'Lovely Earrings',
     description:
       "How could a man resist these lovely earrings? Right - he couldn't.",
@@ -24,7 +24,7 @@ const products = [
     image: 'http://localhost:3100/images/product-earrings.jpg'
   },
   {
-    _id: 'askjll13',
+    //_id: 'askjll13',
     name: 'Working MacBook',
     description:
       'Yes, you got that right - this MacBook has the old, working keyboard. Time to get it!',
@@ -32,14 +32,14 @@ const products = [
     image: 'http://localhost:3100/images/product-macbook.jpg'
   },
   {
-    _id: 'sfhjk1lj21',
+    //_id: 'sfhjk1lj21',
     name: 'Red Purse',
     description: 'A red purse. What is special about? It is red!',
     price: 159.89,
     image: 'http://localhost:3100/images/product-purse.jpg'
   },
   {
-    _id: 'lkljlkk11',
+    //_id: 'lkljlkk11',
     name: 'A T-Shirt',
     description:
       'Never be naked again! This T-Shirt can soon be yours. If you find that buy button.',
@@ -47,7 +47,7 @@ const products = [
     image: 'http://localhost:3100/images/product-shirt.jpg'
   },
   {
-    _id: 'sajlfjal11',
+    //_id: 'sajlfjal11',
     name: 'Cheap Watch',
     description: 'It actually is not cheap. But a watch!',
     price: 299.99,
@@ -57,8 +57,6 @@ const products = [
 
 // Get list of products products
 router.get('/', (req, res, next) => {
-  // const queryPage = req.query.page;
-  // const pageSize = 5;
   // let resultProducts = [...products];
   // if (queryPage) {
   //   resultProducts = products.slice(
@@ -67,20 +65,37 @@ router.get('/', (req, res, next) => {
   //   );
   // }
 
+/*
+  db.getDb().db().collection('products').insertMany(products)
+    .then(result => {
+      res.status(200).json([]);
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'An error occured' });
+    })
+*/
+  const queryPage = req.query.page;
+  const pageSize = 5;
+
   const products = []
-  db.getDb().db().collection('products').find({}).forEach(productDoc => {
+  db.getDb().db().collection('products')
+    .find()
+    .sort({price: -1})
+    .skip((queryPage - 1) * pageSize)
+    .limit(pageSize)
+    .forEach(productDoc => {
         console.log(productDoc)  
         productDoc.price = productDoc.price.toString()      
         products.push(productDoc)
-      })
-      .then(result => {
-        res.status(200).json(products);        
-      })
-      .catch(err => {
-        console.log(err)
-        res.status(500).json({ message: 'An error occured' });
-      })      
     })
+    .then(result => {
+      res.status(200).json(products);        
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({ message: 'An error occured' });
+    })      
+  })
 
 // Get single product
 router.get('/:id', (req, res, next) => {
