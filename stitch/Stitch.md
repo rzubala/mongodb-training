edb08813858ccafd7096dafc652ce08dcdebcc2a
Rafał Zubala <rzubala@operatus.org>
2022-02-12 20:50:34 +0100

    stitch

--- stitch/Stitch.md
+++ stitch/Stitch.md
@@ -1,5 +1,10 @@
 #Stitch
 
-MongoDB Stitch is now labeled "MongoDB Realm".
+[MongoDB Realm](https://realm.mongodb.com/)
 
-Serverless platform for building application. No need to build REST server.
+- MongoDB Stitch is now labeled "MongoDB Realm".
+- Serverless platform for building application. No need to build REST server. It gives access to the database, react to events and execute code in the cloud.
+- It's like Firebase, cloud functions, etc.
+- Stitch servers replace the Node REST server.
+
+
--- stitch/src/pages/Product/Products.js
+++ stitch/src/pages/Product/Products.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+import { ObjectId } from 'bson'
+import { Stitch, RemoteMongoClient } from 'mongodb-stitch-browser-sdk'
 
 import Products from '../../components/Products/Products';
 
@@ -10,8 +11,8 @@ class ProductsPage extends Component {
   }
 
   productDeleteHandler = productId => {
-    axios
-      .delete('http://localhost:3100/products/' + productId)
+    const mongodb = Stitch.defaultAppClient.getServiceClient(RemoteMongoClient.factory, "mongodb-atlas");
+    mongodb.db("shop").collection("products").deleteOne({_id: new ObjectId(productId) })
       .then(result => {
         console.log(result);
         this.fetchData();
@@ -21,20 +22,28 @@ class ProductsPage extends Component {
           'Deleting the product failed. Please try again later'
         );
         console.log(err);
-      });
+      })
   };
 
   fetchData = () => {
-    axios
-      .get('http://localhost:3100/products')
-      .then(productsResponse => {
-        this.setState({ isLoading: false, products: productsResponse.data });
+    const mongodb = Stitch.defaultAppClient.getServiceClient(RemoteMongoClient.factory, "mongodb-atlas");
+    mongodb.db("shop").collection("products").find().asArray()
+      .then(products => {
+        const transformedProducts = products.map(product => {
+          product._id = product._id.toString();
+          product.price = product.price.toString();
+          return product
+        })
+        console.log(products)
+        this.setState({products: transformedProducts, isLoading: false})
       })
       .catch(err => {
-        this.setState({ isLoading: false, products: [] });
-        this.props.onError('Loading products failed. Please try again later');
-        console.log(err);
-      });
+        this.setState({isLoading: false})
+        this.props.onError(
+          'Fetching the products failed. Please try again later'
+        );
+        console.log(err);        
+      })
   };
 
   render() {
